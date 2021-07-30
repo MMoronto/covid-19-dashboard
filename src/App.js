c8a8277a79998fd7258b90d4697f3e22f865750a
MMORONTO <t2wycemo@gmail.com>
2021-07-30 10:59:20 -0400

    layout the structure of our application

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ function App() {
   return (
     <div className="app"> 
       <h1>Let's build a COVID 19 dashboard</h1>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* info box */}
+      {/* info box */}
+      {/* info box */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
