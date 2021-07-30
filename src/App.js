9fafd8ca24c1b765069a6d1e5325d4dd508edf78
MMORONTO <t2wycemo@gmail.com>
2021-07-30 16:53:46 -0400

    style app__header elements

--- src/App.js
+++ src/App.js
@@ -8,19 +8,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app"> 
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app__dropdown">
-        <Select
-          variant="outlined"
-          value="abc"
-        >
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-        </Select>
-      </FormControl>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option two</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">Option iv</MenuItem>
+          </Select>
+        </FormControl>        
+      </div>
+
+
 
       {/* Header */}
       {/* Title + Select input dropdown field */}
