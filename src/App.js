e68f70077e44af19d1afcf7b7e2c23d5723c7765
MMORONTO <t2wycemo@gmail.com>
2021-07-30 16:39:18 -0400

    set up material-ui library. import visual elements. add dropdown menu functionality to UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from "react";
+import {
+  MenuItem, 
+  FormControl, 
+  Select,
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="app"> 
-      <h1>Let's build a COVID 19 dashboard</h1>
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app__dropdown">
+        <Select
+          variant="outlined"
+          value="abc"
+        >
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+        </Select>
+      </FormControl>
 
       {/* Header */}
       {/* Title + Select input dropdown field */}
