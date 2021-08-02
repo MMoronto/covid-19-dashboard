32453f79c68b7996848d788462caa13b75b27574
MMORONTO <t2wycemo@gmail.com>
2021-08-02 15:40:46 -0400

    pull actual numbers from world wide totals. display on respective card components upon selection from dropdown menu

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ function App() {
   const [countryInfo, setCountryInfo] = useState({});
 
   useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(response => response.json())
+    .then(data => {
+      setCountryInfo(data);
+    })
+  }, [])
+
+  useEffect(() => {
     const getCountriesData = async () => {
       await fetch ("https://disease.sh/v3/covid-19/countries")
       .then((response) => response.json())
