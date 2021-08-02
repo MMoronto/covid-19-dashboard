744c2a9c538298882b122baed1367711b5804338
MMORONTO <t2wycemo@gmail.com>
2021-08-02 12:16:47 -0400

    css styling. move app__right card component to right side

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  MenuItem, 
-  FormControl, 
-  Select,
-} from "@material-ui/core";
+import { MenuItem, FormControl, Select, Card, CardContent } from "@material-ui/core";
 import InfoBox from './InfoBox';
 import Map from './Map';
 import './App.css';
@@ -59,8 +55,12 @@ function App() {
         <Map />
       </div>
       <Card className="app__right">
-        {/* Table */}
-        {/* Graph */}        
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          {/* Table */}
+          <h3>Worldwide new cases</h3>
+          {/* Graph */}           
+        </CardContent>       
       </Card>      
     </div>
   );
