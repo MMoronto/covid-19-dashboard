183ddecc80e41e6b8af259f052d62bcfcea8073d
MMORONTO <t2wycemo@gmail.com>
2021-08-02 08:55:13 -0400

    Style elements and props for infobox cards

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -3,19 +3,23 @@ import { Card, CardContent, Typography } from "@material-ui";
 
 function InfoBox({ title, cases, total }) {
 	return (
-		<Card>
+		<Card className="infoBox">
 			<CardContent>
 				{/* Title i.e. Coronavirus cases */}
-				<Typography color="textSecondary">
+				<Typography className="infoBox__title" color="textSecondary">
 					{title}
 				</Typography>
 
 				{/* +120 Number of cases */}
+				<h2 className="infoBox__cases">{cases}</h2>
 
 				{/* 1.2M Total */}
+				<Typography className="infoBox__total" color="textSecondary">
+					{total} Total
+				</Typography>
 			</CardContent>
 		</Card>
-	)
+	);
 }
 
-export default InfoBox
+export default InfoBox;
