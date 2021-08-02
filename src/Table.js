cedf00faf1f2a3ab57d363424ecd170880e6e0f7
MMORONTO <t2wycemo@gmail.com>
2021-08-02 17:21:47 -0400

    pull in table data from api. display in table component card

--- src/Table.js
+++ src/Table.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-function Table({countries }) {
-	return (
-		<div className="table">
-			{countries.map(({country, cases}) => {
-				<tr>
-					<td>{country}</td>
-					<td><strong>{cases}</strong></td>
-				</tr>
-			})}
-		</div>
-	)
+function Table({ countries }) {
+	return <div className="table">
+		{countries.map(({country, cases}) => (
+			<tr>
+				<td>{country}</td>
+				<td><strong>{cases}</strong></td>
+			</tr>
+		))}
+	</div>;
 }
 
 export default Table;
