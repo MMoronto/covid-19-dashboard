4c2552aad8c4d35dc2836033304f4bdc059f6a1d
MMORONTO <t2wycemo@gmail.com>
2021-08-03 08:34:06 -0400

    pull historical data from API

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Line } from "react-chartjs-2";
 
 function LineGraph() {
+	const [data, setData] = useState({});
+
+	// https://disease.sh/v3/covid-19/historical/all?lastdays=120
+
+	useEffect(() => {
+		fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+		.then(response => response.json())
+		.then(data => {
+			// do something to the retrieved data
+		})
+	}, [])
+
 	return (
 		<div>
-
+			<Line data options />
 		</div>
 		)
 }
