a2ab129bd60e37712d86a274c1f18f1f2614dd4c
MMORONTO <t2wycemo@gmail.com>
2021-08-03 10:32:51 -0400

    output API data in correct format. style background

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -6,15 +6,6 @@ function LineGraph() {
 
 	// https://disease.sh/v3/covid-19/historical/all?lastdays=120
 
-	useEffect(() => {
-		fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-		.then(response => response.json())
-		.then(data => {
-			// do something to the retrieved data
-			console.log(data);
-		})
-	}, [])
-
 	const buildChartData = (data, casesType='cases') => {
 		const chartData = [];
 		let lastDataPoint;
@@ -27,16 +18,33 @@ function LineGraph() {
 				chartData.push(newDataPoint);
 			}
 			lastDataPoint = data[casesType][date];
-		})
+		});
 		return chartData;
-	}
+	};
+
+	useEffect(() => {
+		fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+		.then(response => response.json())
+		.then(data => {
+			const chartData = buildChartData(data);
+			setData(chartData);
+		});
+	}, []);
 
 	return (
 		<div>
 			<h1>Graph goes here</h1>
-			{/* <Line data options /> */}
+			<Line data={{
+				datasets: [
+				{
+					backgroundColor: "rgba(204, 16, 52, 0.5)",
+					borderColor: "#CC1034",
+					data: data,
+				},
+			],
+			}} />
 		</div>
-		)
+		);
 }
 
 export default LineGraph
