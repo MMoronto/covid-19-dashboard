c8264f7f3e6675af9bf24f941291e2b68271450c
MMORONTO <t2wycemo@gmail.com>
2021-08-03 23:56:19 -0400

    debug lineGraph issue preventing rendering of chart

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -47,59 +47,58 @@ const options = {
 	},
 };
 
-
-function LineGraph() {
-	const [data, setData] = useState({});
-
-	// https://disease.sh/v3/covid-19/historical/all?lastdays=120
-
-	const buildChartData = (data, casesType = "cases") => {
-		const chartData = [];
-		let lastDataPoint;
-		for(let date in data.cases) {
-		// data[casesType].forEach((date) => {
-			if (lastDataPoint) {
-				const newDataPoint = {
-					x: date,
-					y: data[casesType][date] - lastDataPoint,
-				};
-				chartData.push(newDataPoint);
-			}
-			lastDataPoint = data[casesType][date];
+const buildChartData = (data, casesType = "cases") => {
+	let chartData = [];
+	let lastDataPoint;
+	for(let date in data.cases) {
+		if (lastDataPoint) {
+			let newDataPoint = {
+				x: date,
+				y: data[casesType][date] - lastDataPoint,
+			};
+			chartData.push(newDataPoint);
 		}
-		return chartData;
-	};
+		lastDataPoint = data[casesType][date];
+	}
+	return chartData;
+};
+
+function LineGraph({ casesType = 'cases' }) {
+	const [data, setData] = useState({});
 
 	useEffect(() => {
 		const fetchData = async () => {
 			await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-			.then((response) => response.json())
-			.then((data) => {
-				let chartData = buildChartData(data, 'cases');
-				console.log(chartData);
-
-				setData(chartData);
-			});			
-		}
+				.then((response) => {
+					return response.json();
+				})
+				.then((data) => {
+					let chartData = buildChartData(data, 'cases');
+					setData(chartData);
+				});			
+		};
 		
-	}, []);
+		fetchData();
+	}, [casesType]);
 
 	return (
 		<div>
-			<h1>Graph goes here</h1>
-			<Line 
-			options={options}
-			data={{
-				datasets: [
-				{
-					backgroundColor: "rgba(204, 16, 52, 0.5)",
-					borderColor: "#CC1034",
-					data: data
-				},
-			],
-			}} />
+			{data?.length > 0 && (
+				<Line 
+					data={{
+						datasets: [
+						{
+							backgroundColor: "rgba(204, 16, 52, 0.5)",
+							borderColor: "#CC1034",
+							data: data,
+						},
+					],
+				}} 
+				options={options}
+				/>
+			)}
 		</div>
-		);
+	);
 }
 
-export default LineGraph
+export default LineGraph;
