4784e3571f22792f9b57b85e6ba5fbf77836ac15
MMORONTO <t2wycemo@gmail.com>
2021-08-06 12:36:39 -0400

    style map component. add zoom and center pprops to Map.js

--- src/Map.js
+++ src/Map.js
@@ -5,7 +5,7 @@ import { Map as LeafletMap, TileLayer } from "react-leaflet";
 function Map() {
 	return (
 		<div className="map">
-			<LeafletMap>
+			<LeafletMap center={center} zoom={zoom}>
 				<TileLayer 
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>' />
