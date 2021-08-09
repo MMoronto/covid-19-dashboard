eff0f1adc49b480645d0c4e3241d9a38c16ae1cf
MMORONTO <t2wycemo@gmail.com>
2021-08-09 17:19:47 -0400

    set circle size

--- src/util.js
+++ src/util.js
@@ -5,20 +5,14 @@ import { Circle, Popup } from "react-leaflet";
 const casesTypeColors = {
 	cases: {
 		hex: "#CC1034",
-		rgb: "rgb(204, 16, 52)",
-		half_op: "rgba(204, 16, 52, 0.5)",
 		multiplier: 800,
 	},
 	recovered: {
 		hex: "#7dd71d",
-		rgb: "rgb(125, 215, 29)",
-		half_op: "rgba(125, 215, 29, 0.5)",
 		multiplier: 1200,
 	},
 	deaths: {
 		hex: "#fb4443",
-		rgb: "rgb(251, 68, 67)",
-		half_op: "rgba(251, 68, 67, 0.5)",
 		multiplier: 2000,
 	},		
 };
@@ -35,8 +29,9 @@ export const showDataOnMap = (data, casesType='cases') => (
 		<Circle
 			center={[country.countryInfo.lat, country.countryInfo.long]}
 			fillOpacity={0.4}
-		>
-
-		</Circle>
-	))		
+			color={casesTypeColors[casesType].hex}
+			fillColor={casesTypeColors[casesType].hex}
+			radius={Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier}
+		></Circle>
+	))	
 );
