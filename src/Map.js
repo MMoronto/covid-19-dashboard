610911565cf12a948ad6b43d5033013d11e76559
MMORONTO <t2wycemo@gmail.com>
2021-08-09 14:56:35 -0400

    debug. refactor Map function. resolve issue preventing map from rendering

--- src/Map.js
+++ src/Map.js
@@ -5,11 +5,11 @@ import { MapContainer, TileLayer, useMap } from "react-leaflet";
 function Map({ center, zoom }) {
 	return (
 		<div className="map">
-			<LeafletMap center={center} zoom={zoom}>
+			<MapContainer center={center} zoom={zoom}>
 				<TileLayer 
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>' />
-			</LeafletMap>
+			</MapContainer>
 		</div>
 	);
 }
