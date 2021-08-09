a72470d42f603d4a63e781d2980526892627f3b7
MMORONTO <t2wycemo@gmail.com>
2021-08-09 17:05:46 -0400

    draw circles on map

--- src/util.js
+++ src/util.js
@@ -1,12 +1,21 @@
+import React from "react";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
+
 export const sortData = (data) => {
 	const sortedData = [...data];
 
-	// sortedData.sort((a, b) => {
-	// 	if (a.cases > b.cases) {
-	// 		return -1;
-	// 	} else {
-	// 	return 1;
-	// 	}
-	// })
 	return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1 ));
 };
+
+//Draw circles on the map with interactive tooltip
+export const showDataOnMap = (data, casesType='cases') => (
+	data.map(country => (
+		<Circle
+			center={[country.countryInfo.lat, country.countryInfo.long]}
+			fillOpacity={0.4}
+		>
+
+		</Circle>
+	))		
+);
