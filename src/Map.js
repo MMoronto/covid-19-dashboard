4a1f20a2358780f05d0e42c46c31c3ae768e70a4
MMORONTO <t2wycemo@gmail.com>
2021-08-10 13:47:03 -0400

    refactor mapcontainer component. debug. fix issue preventing map from  matching selected conutry from dropdown list

--- src/Map.js
+++ src/Map.js
@@ -10,14 +10,19 @@ function Map({ countries, casesType, center, zoom }) {
 		return null;
 	}
 	return (
-		<div className="map">
-			<MapContainer center={center} zoom={zoom}>
+			<MapContainer
+				className="map" 
+				casesType={casesType}
+				center={center} 
+				zoom={zoom}
+				scrollWheelZoom={false}
+			>
+				<ChangeView center={center} zoom={zoom} />
 				<TileLayer 
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>' />
 				{showDataOnMap(countries, casesType)}
 			</MapContainer>
-		</div>
 	);
 }
 
